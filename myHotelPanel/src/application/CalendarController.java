74e5c11c964740a5f594edb3cadd799dbff4f24d
e.zagradi <e.zagradi@MININT-DG60DDJ.intranet.bntelectronics.com>
2022-07-23 13:02:47 +0200

    UpDate

---
+++ myHotelPanel/src/application/CalendarController.java
@@ -0,0 +1,65 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.ListView;
+
+public class CalendarController {
+	@FXML
+	private ListView lv_0_1;
+	@FXML
+	private ListView lv_1_1;
+	@FXML
+	private ListView lv_2_1;
+	@FXML
+	private ListView lv_3_1;
+	@FXML
+	private ListView lv_4_1;
+	@FXML
+	private ListView lv_5_1;
+	@FXML
+	private ListView lv_6_1;
+	@FXML
+	private ListView lv_0_2;
+	@FXML
+	private ListView lv_1_2;
+	@FXML
+	private ListView lv_2_2;
+	@FXML
+	private ListView lv_3_2;
+	@FXML
+	private ListView lv_4_2;
+	@FXML
+	private ListView lv_5_2;
+	@FXML
+	private ListView lv_6_2;
+	@FXML
+	private ListView lv_0_3;
+	@FXML
+	private ListView lv_1_3;
+	@FXML
+	private ListView lv_2_3;
+	@FXML
+	private ListView lv_3_3;
+	@FXML
+	private ListView lv_4_3;
+	@FXML
+	private ListView lv_5_3;
+	@FXML
+	private ListView lv_6_3;
+	@FXML
+	private ListView lv_0_4;
+	@FXML
+	private ListView lv_1_4;
+	@FXML
+	private ListView lv_2_4;
+	@FXML
+	private ListView lv_3_4;
+	@FXML
+	private ListView lv_4_4;
+	@FXML
+	private ListView lv_5_4;
+	@FXML
+	private ListView lv_6_4;
+
+}
