68ef4a4f5a5b5a4d1f83f242dbcfad892a897b5c
eljonzagradi <46725455+eljonzagradi@users.noreply.github.com>
2022-07-25 20:33:03 +0200

    In progress

--- myHotelPanel/src/application/CalendarController.java
+++ myHotelPanel/src/application/CalendarController.java
@@ -16,28 +16,11 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CalendarController implements Initializable  {
 	
-	@FXML TableView<Reservation> table;
 	
-	@FXML TableColumn<Reservation,String> nameC;
-	@FXML TableColumn<Reservation,String> surnameC;
-	@FXML TableColumn<Reservation, LocalDate> checkinC;
-	@FXML TableColumn<Reservation, LocalDate> checkoutC;
-	@FXML TableColumn<Reservation, Double> priceC;
-	@FXML TableColumn<Reservation, Integer> roomC;
-	
-	@FXML ListView<Integer> roomList;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
-		nameC.setCellValueFactory(new PropertyValueFactory<>("name"));
-		surnameC.setCellValueFactory(new PropertyValueFactory<>("surname"));
-	
-		checkinC.setCellValueFactory(new PropertyValueFactory<Reservation, LocalDate>("checkin"));
-		checkoutC.setCellValueFactory(new PropertyValueFactory<Reservation, LocalDate>("checkout"));
-		
-		priceC.setCellValueFactory(new PropertyValueFactory<Reservation, Double>("price"));
-		roomC.setCellValueFactory(new PropertyValueFactory<Reservation, Integer>("room"));
 
 	
 		}
