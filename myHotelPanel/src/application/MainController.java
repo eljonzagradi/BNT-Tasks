5c16d595792650f57af06598a23a2e4cd5ba6b62
e.zagradi <e.zagradi@MININT-DG60DDJ.intranet.bntelectronics.com>
2022-07-27 16:30:09 +0200

    Update

--- myHotelPanel/src/application/MainController.java
+++ myHotelPanel/src/application/MainController.java
@@ -2,19 +2,16 @@ package application;
 
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.ResourceBundle;
 
-import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.DateCell;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
@@ -54,16 +51,44 @@ public class MainController implements Initializable {
 	
 	private long currentPrice;
 	
+	public void disableDateCells() 
+	{
+		
+		checkin_x.setDayCellFactory(picker -> new DateCell() {
+        
+			public void updateItem(LocalDate date, boolean empty) {
+            super.updateItem(date, empty);
+            LocalDate today = LocalDate.now();
+
+            setDisable(empty || date.compareTo(today) < 0 );
+            } });
+		
+		checkin_x.valueProperty().addListener( e -> {
+			
+			checkout_x.setDayCellFactory(picker -> new DateCell() {
+		        
+				public void updateItem(LocalDate date, boolean empty) {
+	            super.updateItem(date, empty);
+	            LocalDate checkin = checkin_x.getValue();
+
+	            setDisable(empty || date.compareTo(checkin) < 1 );
+	            } });
+        	
+        	
+        	
+        });
+		
+	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		disableDateCells();
 		tableSetup();
 		setPriceLabel();
-		
 	}
 	
 	public void clickAdd() 
 	{
-		
 		Reservation reservation = 
 				new Reservation(
 						name_x.getText(),
@@ -73,7 +98,6 @@ public class MainController implements Initializable {
 						Reservation.calcPrice(checkin_x, checkout_x, 2000)
 						);
 		reservationsTable.getItems().add(reservation);
-
 	}
 	
 	public void tableSetup() 
@@ -89,7 +113,7 @@ public class MainController implements Initializable {
 	ObservableList<Reservation> getReservationsList() 
 	{
 		ObservableList<Reservation> reservations = FXCollections.observableArrayList();
-		reservations.add(new Reservation("name", "lastname",null, null, 5000));
+		reservations.add(new Reservation("name", "lastname",LocalDate.of(2022,7,27),LocalDate.of(2022,07,31), 10000));
 		return reservations;
 	}
 	
